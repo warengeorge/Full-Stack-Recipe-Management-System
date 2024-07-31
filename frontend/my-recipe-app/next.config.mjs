bae66d02646d23c5789bd5c1fc360b2c1248055a
Tamunomiebi George <georgeomiebi@gmail.com>
2024-07-31 12:33:04 +0100

    add nextConfig

--- frontend/my-recipe-app/next.config.mjs
+++ frontend/my-recipe-app/next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['www.themealdb.com'],
   },
+  reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  }
 };
 
 export default nextConfig;
